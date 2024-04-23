02d5cce16e0f862d65e5b072d82a355e603eb9c3
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-04-23 19:33:06 -0300

    Função de Composição
    Implementação da função de compor uma sequência de batidas.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,18 @@ document.body.addEventListener('keyup', (event) => {
     playSound( event.code.toLowerCase() );
 });
 
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+    //pega o que o usuário digitou e joga em song
+
+    if(song !== '') { //se song não estiver vazio
+        let songArray = song.split('');
+        //cria um array, em que cada item é um caractere
+        //digitado pelo usuário
+        playComposition(songArray);
+    }
+})
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
     //a template string ${} serve pro query selector pegar
@@ -23,5 +35,19 @@ function playSound(sound) {
         }, 300);
         //Remove a classe 300ms depois 
 
+    }
+}
+
+function playComposition(songArray) {
+    let wait = 0;
+
+    for(let songItem of songArray) {
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+            //A ${} serve para tocar a composição conforme a tecla pressionada
+        }, wait);
+        wait += 250;
+
+       
     }
 }
